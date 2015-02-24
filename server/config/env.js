893e03c3ca2f3a7758be8adbadd58fb50dd98dcc
Clark Feusier <cfeusier@gmail.com>
2015-02-24 11:19:23 -0800

    Setup env.js with environment agnostic settings

--- server/config/env.js
+++ server/config/env.js
@@ -1,8 +1,17 @@
+var url = 'http://localhost:8000/';
+var cbUrl = 'http://localhost:8000/api/auth/github/cb';
+
+if (process.env.NODE_ENV === 'production') {
+  url = process.env.APP_URL;
+  cbUrl = process.env.GH_CB_URL;
+}
+
 // Environment Variables
 module.exports = {
   githubOptions: {
     clientID: process.env.GH_CLIENT_ID,
     clientSecret: process.env.GH_CLIENT_SECRET,
-    callbackURL: process.env.GH_CB_URL || 'http://localhost:8000/auth/github/cb'
-  }
+    callbackURL: cbUrl
+  },
+  url: url
 };
