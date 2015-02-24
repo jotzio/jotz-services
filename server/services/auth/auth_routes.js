d44235d5b2b958857c7e4e3d867bc537e0e9f509
Clark Feusier <cfeusier@gmail.com>
2015-02-24 11:24:17 -0800

    Finish basic oauth for gh in auth api

--- server/services/auth/auth_routes.js
+++ server/services/auth/auth_routes.js
@@ -1,5 +1,17 @@
-var authController = require('./auth_controller');
+var auth = require('../auth/auth_api');
+var pp = require('passport');
+
+var ghAuthOptions = {
+  session: false,
+  scope: 'user,gist'
+};
+
+var ghCbOptions = {
+  failureRedirect: '/ghlogin',
+  session: false
+};
 
 module.exports = function(app) {
-
+  app.get('/ghlogin', pp.authenticate('github', ghAuthOptions));
+  app.get('/github/cb', pp.authenticate('github', ghCbOptions), auth.ghOAuthHandler);
 };
