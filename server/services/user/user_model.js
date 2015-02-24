a3ee68f94c8289154e6157cebc8660ebdfd1395f
Clark Feusier <cfeusier@gmail.com>
2015-02-23 18:17:54 -0800

    Update user schema to only include githubId for auth

--- server/services/user/user_model.js
+++ server/services/user/user_model.js
@@ -5,27 +5,25 @@ var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
   appId: { type: String, required: true, inde: { unique: true } },
-  username: { type: String, required: true, index: { unique: true } },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, require: true, unique: true },
+  githubId: { type: String, unique: true },
   salt: { type: String },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
 
 UserSchema.pre('save', function(next) {
-  if (!this.isModified('password')) {
+  if (!this.isModified('githubId')) {
     return next();
   }
   bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
     if (err) {
       return next(err);
     } else {
-      bcrypt.hash(this.password, salt, null, function(err, hash) {
+      bcrypt.hash(this.githubId, salt, null, function(err, hash) {
         if (err) {
           return next(err);
         } else {
-          this.password = hash;
+          this.githubId = hash;
           this.salt = salt;
           next();
         }
@@ -34,9 +32,9 @@ UserSchema.pre('save', function(next) {
   }.bind(this));
 });
 
-UserSchema.methods.comparePassword = function(attempted) {
+UserSchema.methods.compareGithubId = function(attempted) {
   var defer = Q.defer();
-  bcrypt.compare(attempted, this.password, function(err, isMatch) {
+  bcrypt.compare(attempted, this.githubId, function(err, isMatch) {
     if (err) {
       defer.reject(err);
     } else {
