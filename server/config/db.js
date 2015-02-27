0b82309e87a0ba28368c2031a4a32153173da39e
Clark Feusier <cfeusier@gmail.com>
2015-02-26 18:48:39 -0800

    Finish db config file for modularity

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var env = require('./env');
+
+var db = (function() {
+  // Private
+  var registerListeners = function(DB) {
+    DB.on('error', console.error.bind(console, 'mongo connection error:'));
+    DB.on('open', console.log.bind(console, 'mongo hooked'));
+  };
+  var connectDb = function() {
+    mongoose.connect(env.db);
+    return mongoose.connection;
+  };
+
+  // Public
+  return {
+    startDbConnection: function() {
+      registerListeners(connectDb());
+    }
+  };
+})();
+
+module.exports = db;
