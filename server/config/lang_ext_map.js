d4b6aecd32f71140a00fbd202f1a1a86b21abfc9
Clark Feusier <cfeusier@gmail.com>
2015-02-27 17:14:56 -0800

    Add lang_ext_map for gist creation

---
+++ server/config/lang_ext_map.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'coffee': 'coffee',
+  'css': 'css',
+  'sass': 'sass',
+  'html': 'html',
+  'javascript': 'js',
+  'json': 'json',
+  'text': 'txt',
+  'plain_text': 'txt',
+  'jsx': 'jsx',
+  'markdown': 'markdown',
+  'md': 'markdown'
+};
